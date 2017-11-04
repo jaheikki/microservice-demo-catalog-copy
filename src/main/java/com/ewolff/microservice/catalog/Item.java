a5cacf33f087b05895e8699f96e8afd77d0d4471
Heikkila Jari <jari.h.heikkila@tieto.com>
2017-11-04 13:47:05 +0200

    Add ms catalog copy stuff

---
+++ src/main/java/com/ewolff/microservice/catalog/Item.java
@@ -0,0 +1,70 @@
+package com.ewolff.microservice.catalog;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "catalog")
+public class Item {
+
+	private Long id;
+
+	private String name;
+
+	private double price;
+
+	public Item() {
+		super();
+		id = 0l;
+	}
+
+	public Item(Long id , String name, double price) {
+		super();
+		this.id = id; 
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+}
